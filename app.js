05bc68893eb9bbd3f2f996943722bdb595001ba3
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-07-14 10:32:40 -0700

    added functions and fixed some bugs

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 'use strict';
 
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function validateYesNoInput(input) {
+  return input.toLowerCase() === 'yes' || input.toLowerCase() === 'no';
+}
+
+function displayGameOver(answer) {
+  alert('Game is over! The number is: ' + answer);
+}
+
+
+
 let question1 ='Do I know how to snowboard?';
 let question2 ='Am I a good cook?';
 let question3 ='Have I ever been to India?';
@@ -17,28 +31,28 @@ let name = prompt('What is your name?');
 alert('Hi '+ name + '!');
 let count = 0;
 
-for (let i = 0; i < questions.length; i++) {
-  let userInput = prompt(questions[i]).toLowerCase();
-  if (userInput !== 'yes' && userInput !== 'no') {
-    alert('Please answer with \'yes\' or \'no\'.');
-    i--;
-  } else {
-    responses.push(userInput);
-    if (responses[i] === answers[i]) {
-      // console.log('You are right!');
-      alert('You are right!' + explanations[i]);
-      count++;
-    } else {
-      // console.log('Oops, you missed this one.');
-      alert('Oops, you missed this one.' + explanations[i]);
-    }
-  }
-}
+// for (let i = 0; i < questions.length; i++) {
+//   let userInput = prompt(questions[i]).toLowerCase();
+//   if (!validateYesNoInput(userInput)) {
+//     alert('Please answer with \'yes\' or \'no\'.');
+//     i--;
+//   } else {
+//     responses.push(userInput);
+//     if (responses[i] === answers[i]) {
+//       // console.log('You are right!');
+//       alert('You are right!' + explanations[i]);
+//       count++;
+//     } else {
+//       // console.log('Oops, you missed this one.');
+//       alert('Oops, you missed this one.' + explanations[i]);
+//     }
+//   }
+// }
 
-let answer6 = 6;
+let answer6 = getRandomNumber(1, 10);
 let response6 = prompt(question6);
 let attempt6 = 4;
-while ( attempt6 > 1) {
+while ( attempt6 >= 1) {
   if (parseInt(response6) === answer6) {
     count++;
     alert('Congratulations! The number is: ' + answer6);
@@ -46,20 +60,28 @@ while ( attempt6 > 1) {
   } else if (parseInt(response6) > answer6) {
     attempt6--;
     alert('Too high! Try again. You have ' + attempt6 + ' attempts left.');
-    response6 = prompt(question6);
+    if (attempt6 > 0) {
+      response6 = prompt(question6);
+    } else {
+      displayGameOver(answer6);
+      break;
+    }
   } else if (parseInt(response6) < answer6) {
     attempt6--;
     alert('Too low! Try again. You have ' + attempt6 + ' attempts left.');
-    response6 = prompt(question6);
-  } else if ( (!response6) || (!Number((response6)))) {
-    alert('Please enter a number.');
+    if (attempt6 > 0) {
+      response6 = prompt(question6);
+    } else {
+      displayGameOver(answer6);
+      break;
+    }
+  } else if (!response6 || !Number(response6)) {
+    alert('Please enter a valid number.');
     response6 = prompt(question6);
   }
 }
 
-if (attempt6 === 1) {
-  alert('Game is over! The number is: ' + answer6);
-}
+
 
 let newAnswers7 = [];
 for (let i=0; i < answers7.length; i++) {
@@ -69,7 +91,7 @@ for (let i=0; i < answers7.length; i++) {
 let correct = false;
 let attempt7 = 6;
 let response7 = prompt(question7);
-while (attempt7 >= 1) {
+while (attempt7 >= 1 && response7) {
   for (let i=0; i < answers7.length; i++) {
     if (response7.toLowerCase() === answers7[i]) {
       correct = true;
